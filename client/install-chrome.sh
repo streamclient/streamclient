5481c4574e8d7505dc484ea2a83d2dbd112c4510
Kushal <kushaldeveloper@gmail.com>
2018-10-04 15:09:20 -0400

    there is no sudo
    https://gitlab.com/streamlabs/streamviewer/-/jobs/104836033

--- client/install-chrome.sh
+++ client/install-chrome.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 
 if ! which google-chrome > /dev/null; then
-sudo su << EOF
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 apt-get update && apt-get -y install google-chrome-stable
-EOF
 fi
