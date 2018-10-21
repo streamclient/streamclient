eb718e20375654d01ca108afe7bfb8b59c8140fc
Kushal Hada <kushaldeveloper@gmail.com>
2018-10-21 17:48:04 -0400

    update to angular 7

--- client/install-chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-if ! which google-chrome > /dev/null; then
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-apt-get update && apt-get -y install google-chrome-stable
-fi
--- client/src/karma.conf.js
+++ client/src/karma.conf.js
@@ -25,21 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          '--no-sandbox',
-          '--remote-debugging-port=9222',
-        ]
-      }
-    },
-    browsers: [
-      'Chrome',
-      'ChromeHeadless',
-    ],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
